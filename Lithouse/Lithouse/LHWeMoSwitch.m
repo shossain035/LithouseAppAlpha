9d1ce716eeb6fcc1ceec770f90f734b24685ee4f
shossain035 <shah@mac-mini.gateway.2wire.net>
2014-05-29 12:38:34 -0700

    wemo icon from app

--- Lithouse/Lithouse/LHWeMoSwitch.m
+++ Lithouse/Lithouse/LHWeMoSwitch.m
@@ -34,15 +34,8 @@
 {
     self.weMoControlDevice = aWeMoControlDevice;
     self.friendlyName = aWeMoControlDevice.friendlyName;
-        
-    if (aWeMoControlDevice.icon == nil) {
-        self.displayImage = [self defaultDeviceIcon : aWeMoControlDevice.deviceType];
-    }
-    else {
-        //todo: find a better solution for white background
-        self.displayImage = [self changeWhiteColorTransparent : aWeMoControlDevice.icon];
-    }
-        
+    self.displayImage = [self defaultDeviceIcon : aWeMoControlDevice.deviceType];
+    
     if ( self.weMoControlDevice.state == WeMoDeviceOn ) {
         self.currentStatus = LHDeviceIsOn;
     } else {
@@ -69,28 +62,6 @@
     }
     
     return nil;
-}
-
-- (UIImage *) changeWhiteColorTransparent : (UIImage *) image
-{
-    CGImageRef rawImageRef = image.CGImage;
-    
-    const float colorMasking[6] = {222, 255, 255, 255, 255, 255};
-    
-    UIGraphicsBeginImageContext ( image.size );
-    CGImageRef maskedImageRef=CGImageCreateWithMaskingColors(rawImageRef, colorMasking);
-    CGContextTranslateCTM(UIGraphicsGetCurrentContext(), 0.0, image.size.height);
-    CGContextScaleCTM(UIGraphicsGetCurrentContext(), 1.0, -1.0);
-    
-    CGContextDrawImage(UIGraphicsGetCurrentContext(),
-                       CGRectMake(0, 0, image.size.width, image.size.height),
-                       maskedImageRef);
-    
-    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
-    CGImageRelease(maskedImageRef);
-    UIGraphicsEndImageContext();
-    
-    return result;
 }
 
 - (void) turnOn
